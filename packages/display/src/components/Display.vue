a616feef6c1f83e793a8191e9ad516846800008a
rkusan00 <renato.kusanovic@gmail.com>
2024-12-05 14:33:21 +0100

    Tweak display container

--- packages/display/src/components/Display.vue
+++ packages/display/src/components/Display.vue
@@ -1,87 +1,108 @@
 <template>
-  <VForm ref="form" class="tce-root" @submit.prevent="submit">
-    <EmbeddedContainer :elements="questionPrompt" />
-    <div v-if="data.hint" class="d-flex justify-end mb-4">
-      <VTooltip
-        v-model="showHint"
-        :open-on-hover="false"
-        location="bottom"
-        max-width="350"
-        close-on-back
-        open-on-click
-      >
-        <template #activator="{ isActive, props: tooltipProps }">
-          <VBtn
-            v-click-outside="() => (showHint = false)"
-            v-bind="tooltipProps"
-            :active="isActive"
-            :prepend-icon="`mdi-lightbulb-${isActive ? 'on' : 'outline'}`"
-            size="small"
-            text="Hint"
-            variant="text"
-            rounded
-          />
-        </template>
-        {{ data.hint }}
-      </VTooltip>
-    </div>
-    <VInput
-      :rules="[requiredRule]"
-      :validation-value="selectedAnswer !== null"
-      hide-details="auto"
-      validate-on="submit"
-    >
-      <VItemGroup
-        v-model="selectedAnswer"
-        class="w-100"
-        selected-class="bg-blue-grey-lighten-4"
-        mandatory
-      >
-        <VItem
-          v-for="(item, index) in data.answers"
-          :key="index"
-          v-slot="{ toggle, isSelected, selectedClass }"
-          :value="index"
+  <VCard class="question-container pa-4">
+    <VForm ref="form" class="tce-root" @submit.prevent="submit">
+      <TailorEmbeddedContainer :elements="questionPrompt" class="mb-4" />
+      <div class="d-flex align-center text-subtitle-2 mb-2">
+        Select one:
+        <VSpacer />
+        <VTooltip
+          v-if="data.hint"
+          v-model="showHint"
+          :open-on-hover="false"
+          location="bottom"
+          max-width="350"
+          close-on-back
+          open-on-click
         >
-          <VCard
-            :class="selectedClass"
-            :disabled="submitted"
-            class="d-flex align-center px-4 py-3 mb-3"
-            color="blue-grey-darken-2"
-            rounded="lg"
-            variant="outlined"
-            @click="toggle"
-          >
-            <VAvatar
-              :class="{ 'font-weight-bold': isSelected }"
-              :variant="isSelected ? 'flat' : 'outlined'"
-              class="mr-4"
-              color="blue-grey-darken-2"
+          <template #activator="{ isActive, props: tooltipProps }">
+            <VBtn
+              v-click-outside="() => (showHint = false)"
+              v-bind="tooltipProps"
+              :active="isActive"
+              :prepend-icon="`mdi-lightbulb-${isActive ? 'on' : 'outline'}`"
               size="small"
+              text="Hint"
+              variant="text"
+              rounded
+            />
+          </template>
+          {{ data.hint }}
+        </VTooltip>
+      </div>
+      <VInput
+        :rules="[requiredRule]"
+        :validation-value="selectedAnswer !== null"
+        hide-details="auto"
+        validate-on="submit"
+      >
+        <VItemGroup
+          v-model="selectedAnswer"
+          class="w-100 d-flex flex-column ga-2"
+          selected-class="bg-blue-grey-lighten-5"
+          mandatory
+        >
+          <VItem
+            v-for="(item, index) in data.answers"
+            :key="index"
+            v-slot="{ toggle, isSelected, selectedClass }"
+            :value="index"
+          >
+            <VCard
+              :class="selectedClass"
+              :disabled="submitted"
+              class="d-flex align-center px-4 py-3"
+              border
+              flat
+              rounded
+              @click="toggle"
             >
-              {{ index + 1 }}
-            </VAvatar>
-            {{ item }}
-            <VSpacer />
-            <template v-if="submitted && 'isCorrect' in userState">
-              <VIcon v-if="isSelected" v-bind="iconProps(index)" />
-            </template>
-          </VCard>
-        </VItem>
-      </VItemGroup>
-    </VInput>
-    <VAlert
-      v-if="submitted"
-      v-bind="alertProps"
-      class="mb-3"
-      rounded="lg"
-      variant="tonal"
-    />
-    <div class="d-flex justify-end">
-      <VBtn v-if="!submitted" type="submit" variant="tonal">Submit</VBtn>
-      <VBtn v-else variant="tonal" @click="submitted = false">Try Again</VBtn>
-    </div>
-  </VForm>
+              <VAvatar
+                :class="{ 'font-weight-bold': isSelected }"
+                :variant="isSelected ? 'flat' : 'outlined'"
+                class="mr-4"
+                color="blue-grey-darken-2"
+                size="small"
+              >
+                {{ index + 1 }}
+              </VAvatar>
+              {{ item }}
+              <VSpacer />
+              <template v-if="submitted && 'isCorrect' in userState">
+                <VIcon v-if="isSelected" v-bind="iconProps(index)" />
+              </template>
+            </VCard>
+          </VItem>
+        </VItemGroup>
+      </VInput>
+      <VDivider class="my-4 mx-n4" />
+      <VAlert
+        v-if="submitted"
+        v-bind="alertProps"
+        class="mb-3"
+        rounded="lg"
+        variant="tonal"
+      />
+      <div class="d-flex justify-end">
+        <VBtn
+          v-if="!submitted"
+          color="primary"
+          prepend-icon="mdi-check"
+          type="submit"
+          variant="tonal"
+        >
+          Submit
+        </VBtn>
+        <VBtn
+          v-else
+          prepend-icon="mdi-refresh"
+          variant="tonal"
+          @click="submitted = false"
+        >
+          Retry
+        </VBtn>
+      </div>
+    </VForm>
+  </VCard>
 </template>
 
 <script setup lang="ts">
