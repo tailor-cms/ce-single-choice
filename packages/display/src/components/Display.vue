00135eb00641117baf3501f69a62c0b37207674d
rkusan00 <renato.kusanovic@gmail.com>
2025-07-05 17:24:35 +0200

    Update display package

--- packages/display/src/components/Display.vue
+++ packages/display/src/components/Display.vue
@@ -1,6 +1,7 @@
 <template>
   <QuestionContainer
     :data="element.data"
+    :feedback="feedback"
     :is-correct="userState.isCorrect"
     :is-graded="isGraded"
     :is-submitted="isSubmitted"
@@ -24,23 +25,23 @@
         <VItem
           v-for="(item, index) in element.data.answers"
           :key="index"
-          v-slot="{ toggle, isSelected, selectedClass }"
+          v-slot="{ toggle, isSelected }"
           :value="index"
         >
           <VCard
-            :class="selectedClass"
-            :disabled="isSubmitted"
+            v-bind="isSubmitted ? {} : { onClick: toggle }"
+            :class="{ readonly: isSubmitted, selected: isSelected }"
+            :color="isSelected ? 'primary-darken-1' : 'white'"
+            :variant="isSelected ? 'tonal' : 'flat'"
             class="d-flex align-center px-4 py-3"
             border
-            flat
             rounded
-            @click="toggle"
           >
             <VAvatar
               :class="{ 'font-weight-bold': isSelected }"
               :variant="isSelected ? 'flat' : 'outlined'"
               class="mr-4"
-              color="blue-grey-darken-2"
+              color="primary-darken-1"
               size="small"
             >
               {{ indexToAlpha(index) }}
@@ -64,19 +65,25 @@
 <script setup lang="ts">
 import { computed, ref, watch } from 'vue';
 import { Element } from '@tailor-cms/ce-single-choice-manifest';
+import { pick } from 'lodash-es';
 import { QuestionContainer } from '@tailor-cms/lx-components';
 
 const props = defineProps<{ element: Element; userState: any }>();
 const emit = defineEmits(['interaction']);
 
 const isSubmitted = ref(!!props.userState.isSubmitted);
-const selectedAnswer = ref<string>(props.userState.response ?? null);
+const selectedAnswer = ref<number>(props.userState.response ?? null);
 
 const isGraded = computed(() => 'isCorrect' in props.userState);
 
 const indexToAlpha = (index: number) => String.fromCharCode(index + 65);
 const isCorrect = (index: number) => props.userState.correct === index;
 const submit = () => emit('interaction', { response: selectedAnswer.value });
+const feedback = computed(() => {
+  const feedback = props.element.data.feedback;
+  if (selectedAnswer.value === null) return;
+  return pick(feedback, selectedAnswer.value);
+});
 
 const requiredRule = (val: number) =>
   typeof val === 'number' || 'You have to select an answer';
@@ -90,3 +97,9 @@ watch(
   { deep: true },
 );
 </script>
+
+<style lang="scss" scoped>
+.v-input .selected.v-card {
+  border: 1px solid color-mix(in srgb, currentColor 36%, transparent);
+}
+</style>
